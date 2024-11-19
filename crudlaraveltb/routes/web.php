c8a03ef2774f60ec09a0e0a5067fd4a44d5cd301
Diego <diego.lopes.tinfem2021@gmail.com>
2024-11-19 10:25:16 -0300

    deu errinho nas rotas, arrumei

--- crudlaraveltb/routes/web.php
+++ crudlaraveltb/routes/web.php
@@ -25,8 +25,6 @@ Route::group(['namespace' => 'App\Http\Controllers'], function () {
      */
     Route::get('/', 'HomeController@index')->name('home.index');
 
-AgoraSemErros
-
     Route::post('/alunos/store2', [AlunosController::class, 'store2'])->name('alunos.store2');  // Salvar novo aluno
 
     Route::post('/alunos/check-duplicate', [AlunosController::class, 'checkDuplicate'])->name('alunos.checkDuplicate');
